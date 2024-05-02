cfafb41d688c77ac71405be23097f0007985906a
IsabelleCarvalho2007 <166523028+IsabelleCarvalho2007@users.noreply.github.com>
2024-05-02 11:04:25 -0300

    Update calculadoradedesconto.py

--- calculadoradedesconto.py
+++ calculadoradedesconto.py
@@ -1,4 +1,4 @@
-preço=float(input("digite o preço:"))
-desconto=float(input("digite o seu desconto:"))
-valor= preço-(preço*desconto/100)
-print(valor)
+preço = float(input("Digite o preço:"))
+desconto = float(input("digite o seu desconto:"))
+valor = preço-(preço*desconto/100)
+print("O valor é: ")
