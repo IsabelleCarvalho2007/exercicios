cab7db138164e6f5525a2e87beb3a8ebb6f22fa3
IsabelleCarvalho2007 <166523028+IsabelleCarvalho2007@users.noreply.github.com>
2024-05-02 10:59:47 -0300

    Update Juros simples.py

--- Juros simples.py
+++ Juros simples.py
@@ -1,5 +1,5 @@
-capital=float(input("Digite o capital inicia:"))
+capital=float(input("Digite o capital inicial:"))
 tempo=float(input("Digite o tempo de aplicação em meses:"))
 taxa=float(input("Digite a taxa:"))
 juros=(capital*taxa*tempo)/100
-print("Total ganho em juros mensal é:",juros,)
+print("Total ganho em juros mensal é: ",juros,)
