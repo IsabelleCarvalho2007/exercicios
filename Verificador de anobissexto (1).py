f7e548da94ad636e7d5d5c2ad16f402852472697
IsabelleCarvalho2007 <166523028+IsabelleCarvalho2007@users.noreply.github.com>
2024-05-02 10:48:30 -0300

    Verificador de anobissexto (1).py

--- Verificador de anobissexto (1).py
+++ Verificador de anobissexto (1).py
@@ -1,5 +1,5 @@
-print("bom dia princesa")
-ano = int(input("ano:"))
+print("bom dia ")
+ano = int(input("Digite um ano para verificar:"))
 if (ano%4==0 and ano%100!=0) or (ano%400==0):
     print("é ano bissexto")
 else:
